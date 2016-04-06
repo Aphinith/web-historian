1b132422a5474a5a01ed684712e4149da4cab6e7
aphinith <aphinith84@gmail.com>
2016-04-06 16:19:22 -0700

    Server page works, now trying to pass first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,27 +1,29 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers.js');
+var fs = require('fs');
 // require more modules/folders here!
 
-var headers = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  "Content-Type": "application/JSON"
-};
 
 exports.handleRequest = function (req, res) {
 
-  console.log("Serving request type " + request.method + " for url " + request.url);
+  console.log("Serving request type " + req.method + " for url " + req.url);
   var statusCode;
 
-  if (request.method === 'GET') {
-    statusCode = 200;
-    response.writeHead(statusCode, headers);
-    console.log('the request-handler page is also working');
-
-    response.end(nothing);
-    // response.end(archive.paths.list);
+  if (req.method === 'GET') {
+   fs.readFile(__dirname + req.url, function (err, data) {
+    console.log('dirname:', __dirname);
+    console.log('err:', err);
+    console.log('data:', data);
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+   })
 
   };
+
 };
