619013847ad10a6078c402804bc7fd8a7053a859
aphinith <aphinith84@gmail.com>
2016-04-06 17:27:05 -0700

    Working on 2nd test

--- web/request-handler.js
+++ web/request-handler.js
@@ -10,11 +10,9 @@ exports.handleRequest = function (req, res) {
   console.log("Serving request type " + req.method + " for url " + req.url);
   var statusCode;
 
-  if (req.method === 'GET') {
+
+  if (req.method === 'GET' && req.url === '/') {
    fs.readFile(__dirname + '/public/index.html', function (err, data) {
-    console.log('dirname:', __dirname + '/public/index.html');
-    console.log('err:', err);
-    console.log('data:', data);
     if (err) {
       res.writeHead(404);
       res.end(JSON.stringify(err));
@@ -24,6 +22,19 @@ exports.handleRequest = function (req, res) {
     res.end(data);
    })
 
+  } else {
+    fs.readdir(__dirname + '/archives/sites', function (err, path) {
+      console.log('this error is here:', err);
+      console.log('path:', path);
+
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+   })
   };
 
 };
